1cd93691522e479d86ecf9941f582a13f0e233ea
jerin.t <jerin.t@iprotecs.net>
2024-08-29 12:10:54 +0530

    get Group msg added phone and givenName correction

--- controllers/Groups/messages.js
+++ controllers/Groups/messages.js
@@ -1,6 +1,7 @@
 const { JersApp_Auth } = require("../../model/auth");
 const { JersApp_Group } = require("../../model/Groups/group");
 const { JersApp_grp_message } = require("../../model/Groups/message");
+const { getContactByUserID } = require("../../services/contacts");
 const { authenticateByTokenAndUserID } = require("../../utils/Authentication");
 
 exports.getMessages = async (req, res, next) => {
@@ -22,8 +23,14 @@ exports.getMessages = async (req, res, next) => {
           let messages = [];
           for (let msg of group.messages) {
             const user = await JersApp_Auth.findById(msg.sender_id);
-            const { image, name, ...otherDatas } = user.toObject();
-            const newObj = Object.assign({}, msg.toObject(), { name, image });
+            const contact = await getContactByUserID(msg.sender_id, userID);
+            const { image, name, mobNum, ...otherDatas } = user.toObject();
+            const newObj = Object.assign({}, msg.toObject(), {
+              name,
+              image,
+              given_name: contact.given_name,
+              phone: mobNum,
+            });
             messages.push(newObj);
           }
           res.status(200).json({ status: "ok", data: messages });
